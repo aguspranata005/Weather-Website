2c1816e3bc84277efeb115c53f5de5bf1a7cc40a
resource008 <162302443+aguspranata005@users.noreply.github.com>
2025-10-26 00:39:08 +0700

    update frontend and delete json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -52,7 +52,9 @@ interface AirPollutionResponse {
 }
 
 // --- Konfigurasi & Fungsi Helper ---
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = process.env.NODE_ENV === 'production'
+  ? '/api'
+  : 'http://localhost:8080/api';
 const getWeatherIconUrl = (iconCode: string): string => {
   const baseUrl = "https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/";
   let iconName: string;
